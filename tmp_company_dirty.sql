f7f7e00463088ec2c6d27ba72e00a5e301dcccaf
hepanblock-hub <hepanblock@gmail.com>
2026-04-07 15:24:08 +0800

    chore: gitignore tmp sql files and dev logs

--- tmp_company_dirty.sql
+++
@@ -1,6 +0,0 @@
-SELECT company_name, COUNT(*) AS cnt
-FROM company_pages
-WHERE company_name ILIKE '%select%'
-GROUP BY 1
-ORDER BY cnt DESC, company_name
-LIMIT 30;
--- tmp_company_slugs.sql
+++
@@ -1,4 +0,0 @@
-SELECT slug,state FROM company_pages
-WHERE slug IS NOT NULL AND slug<>''
-ORDER BY id DESC
-LIMIT 10;
